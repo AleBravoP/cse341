ea5b1b20ab4dbc2b76fc7f57eae4f6f17dc0f5e4
ale <bra21043@byui.edu>
2024-03-13 08:00:08 -0600

    swagger added

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ const port = process.env.PORT || 3000;
 // body parser for users controller to work
 app.use(bodyParser.json());
 
+// allow routes to work accross sites
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Contente-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', require('./routes'));
 
 mongodb.initDb((err)=> {
